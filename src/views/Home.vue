b13f2adbd925cfcffb8092e79711b26bbb1cc76e
kaczor6418 <andrzej6418@gmail.com>
2020-04-24 20:57:07 +0200

    refactor(project): some code improvement

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <HelloWorld msg="TRACKVID"/>
+    </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+    import HelloWorld from "@/components/HelloWorld.vue";
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    @Component({
+        components: {
+            HelloWorld
+        }
+    })
+    export default class Home extends Vue {
+
+    }
 </script>
