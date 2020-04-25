9b845b6a865cfe97a74cee4bf954becb42aefb74
kaczor6418 <andrzej6418@gmail.com>
2020-04-25 22:19:02 +0200

    feat(Tracking): track button save localization data

--- src/components/trackButton/TrackButton.vue
+++ src/components/trackButton/TrackButton.vue
@@ -1,25 +1,38 @@
+import {TrackButtonState} from "@/components/trackButton/interface/TrackButtonState";
 <template>
     <div>
-        <div class="button-wrapper">
-          <div class="circle"><b>{{trackButtonText}}</b></div>
+        <div class="button-wrapper" @click="toggleTracking">
+            <div class="circle" :class="{'circle--start': isTrackingInActive(), 'circle--stop': isTrackingActive()}"><b>{{trackButtonState}}</b></div>
         </div>
     </div>
 </template>
 
 <script lang="ts">
     import {Component, Prop, Vue} from 'vue-property-decorator';
-    import {TrackButtonState} from "@/components/trackButton/TrackButtonState";
+    import {TrackButtonState} from "@/components/trackButton/interface/TrackButtonState";
+    import {TrackingService} from "@/core/trackingService/TrackingService";
+    import {TGeoSuccess} from "@/core/trackingService/interfaces/TGeoSuccess";
+    import {MeasurementsState, TLocation} from "@/store/state/MeasurementsState";
+    import {Utils} from "@/utils/Utils";
+    import {TTrackServiceOptions} from "@/components/trackButton/interface/TTrackServiceOptions";
 
     @Component
     export default class TrackButton extends Vue {
-        @Prop() private size!: string;
+        @Prop() private trackingOptions!: TTrackServiceOptions;
 
         private static CIRCLE_WRAPPER_CLASS_NAME = 'button-wrapper';
 
-        public trackButtonText: TrackButtonState = TrackButtonState.START;
+        public trackButtonState: TrackButtonState = TrackButtonState.START;
+
+        private readonly size: string;
+        private readonly trackingInterval: number;
+        private trackingService: TrackingService = new TrackingService(TrackButton.saveNewPosition, {timeout: 10000});
+        private intervalPid: number = 0;
 
         constructor() {
             super();
+            this.size = this.trackingOptions.size;
+            this.trackingInterval = this.trackingOptions.trackInterval;
         }
 
         mounted(): void {
@@ -28,16 +41,61 @@
                 circleContainer.style.width = this.size;
             }
         }
+
+        public toggleTracking(): void {
+            if (this.trackButtonState === TrackButtonState.START) {
+                this.trackButtonState = TrackButtonState.STOP;
+                this.trackUser();
+                return;
+            }
+            if (this.trackButtonState === TrackButtonState.STOP) {
+                this.trackButtonState = TrackButtonState.START;
+                clearInterval(this.intervalPid);
+            }
+        }
+
+        private trackUser(): void {
+            this.intervalPid = window.setInterval(this.trackingService.saveCurrentPosition.bind(this.trackingService), 10000);
+
+        }
+
+        public isTrackingActive(): boolean {
+            return this.trackButtonState === TrackButtonState.STOP;
+        }
+
+        public isTrackingInActive(): boolean {
+            return this.trackButtonState === TrackButtonState.START;
+        }
+
+        private static saveNewPosition(newPosition: TGeoSuccess): void {
+            const stateDTO: string | null = window.localStorage.getItem('measurements');
+            if (stateDTO == null) {
+                window.localStorage.setItem('measurements', JSON.stringify([]));
+            }
+            if (stateDTO != null) {
+                const location: TLocation = {
+                    latitude: newPosition.latitude,
+                    longitude: newPosition.longitude
+                };
+                const date: string = Utils.buildDateString('-');
+                const newItem: MeasurementsState = {
+                    location,
+                    date
+                };
+                const measurements: MeasurementsState[] = JSON.parse(stateDTO);
+                measurements.push(newItem);
+                window.localStorage.setItem('measurements', JSON.stringify(measurements));
+            }
+        }
     }
 </script>
 
 <style scoped lang="scss">
     .button-wrapper {
-      margin: auto;
+        margin: auto;
     }
 
     .circle {
-        background: var(--accent-color-one);
         color: var(--text-color-on-accent);
         box-shadow: 0 0 8px rgba(0, 0, 0, 0.5);
 
@@ -53,9 +111,18 @@
 
         cursor: pointer;
         transition: all 0.5s ease-in-out;
+
+        &--start {
+            background: var(--accent-color-one);
+        }
+
+        &--stop {
+            background: var(--accent-color-two);
+        }
+
+        &:hover {
+            transform: scale(1.2);
+        }
     }
 
-    .circle:hover {
-        transform: scale(1.1);
-    }
 </style>
