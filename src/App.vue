4c8b881b1558196e50e0c63a2e31ce25f5e17a4d
kaczor6418 <andrzej6418@gmail.com>
2020-04-26 11:40:46 +0200

    fix(App): add typescript to App

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@
         <router-view/>
     </div>
 </template>
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
 
+    @Component
+    export default class App extends Vue{
+
+    }
+</script>
 <style lang="scss">
   @import "styles/css-reset";
   @import "styles/color.variables";
