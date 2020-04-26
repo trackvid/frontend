2f08d1b5f9c9bf33db2776a7778ec78b5941e89f
kaczor6418 <andrzej6418@gmail.com>
2020-04-26 17:53:43 +0200

    fix(leaflet): ugly needed leaflet hack

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,16 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
+
+// Because leaflet can not find markers
+delete (<any>Icon.Default.prototype)._getIconUrl;
+Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 Vue.config.productionTip = false;
 
